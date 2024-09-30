1af73aff33a578a18b9ba71e4f793d6c62a7f82b
manuelsabe <manuirigoyen@hotmail.com>
2024-09-30 19:05:38 -0300

    agg nuevos ej

---
+++ invertirarreglo.ts
@@ -0,0 +1,25 @@
+import * as rs from "readline-sync";
+
+console.log( `        
+    Creacion del arreglo:   `);
+
+let dimension : number = rs.questionInt("Ingrese que dimension quiere para el arreglo: ");
+
+let arr : number [] = new Array (dimension);
+
+
+for (let i : number = 0; i < arr.length; i++){
+
+    arr [i] = rs.questionInt("Ingrese el numero que desea: ")
+    console.log(arr[i]);
+
+}
+
+for(let i : number = dimension -1; i>=0; i--){
+
+       
+        console.log( `     
+            ${arr[i]}  `); 
+
+}
+
---
+++ negpos.ts
@@ -0,0 +1,25 @@
+import * as rs from "readline-sync";
+
+let longitud : number = rs.questionInt("ingrese longitud del arreglo: ")
+let negativos : number = 0
+let positivos : number = 0
+let ceros : number = 0
+let arreglosnumeros : number [] = new Array(longitud);
+
+for (let i : number = 0; i < longitud; i++){
+
+        arreglosnumeros[i] = rs.questionInt("ingrese numero para el arreglo: ");
+
+        if (arreglosnumeros[i] > 0){
+            positivos++; //positivos = positivos + 1
+        }
+        else if (arreglosnumeros[i] < 0){
+            negativos++
+        }
+        else{
+            ceros++;
+        }
+
+}
+
+console.log( ` Hay ${positivos} positivos, ${negativos} negativos, ${ceros} ceros`);
